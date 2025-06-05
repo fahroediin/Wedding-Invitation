7b9e6c42a2866deb1fc48b26eb8ba9623d04852b
Fahrudin <fahroediinj@gmail.com>
2025-06-05 09:45:44 +0700

    config edit

--- src/config/config.js
+++ src/config/config.js
@@ -1,38 +1,38 @@
 // Nanti convert ke Bahasa Indonesia
 const config = {
   data: {
-    title: "Pernikahan Fulan & Fulana", // Nanti ini otomatis
+    title: "Pernikahan Henky & Fadhli", // Nanti ini otomatis
     description:
       "Kami akan menikah dan mengundang Anda untuk turut merayakan momen istimewa ini.", // Nanti ini dibikin random
-    groomName: "Fulan",
-    brideName: "Fulana",
+    groomName: "Henky",
+    brideName: "Fadhli",
     parentGroom: "Bapak Groom & Ibu Groom",
     parentBride: "Bapak Bride & Ibu Bride",
-    date: "2024-12-24",
+    date: "2025-06-06",
     maps_url: "https://goo.gl/maps/abcdef",
     maps_embed:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.0000000000005!2d106.8270733147699!3d-6.175392995514422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f4f1b6d7b1e7%3A0x2e69f4f1b6d7b1e7!2sMonumen%20Nasional!5e0!3m2!1sid!2sid!4v1633666820004!5m2!1sid!2sid",
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.0000000000005!2d106.8270733147699!3d-6.175392995514422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f4f1b6d7b1e7%3A0x2e69f4f1b6d7b1e7!2sMonumen%20Nasional!5e0!3m2!1sid!2sid!4v1633666820004!5m2!1https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2352.8602351040663!2d108.8042666398272!3d-7.351260187304336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f7f0016b1ecd9%3A0xae0491d1cfc26e04!2sAlfamart%20Karangreja!5e0!3m2!1sen!2sid!4v1749090390582!5m2!1sen!2sidsid!2sid",
     time: "16:16 - 17:30 WIB",
-    location: "Grand Ballroom, Hotel Majesty",
-    address: "Jl. Jend. Sudirman No.1, Jakarta",
+    location: "Alfamart Karangreja",
+    address: "Tobong, Kec. Cimanggu Kabupaten Cilacap",
     ogImage: "/images/og-image.jpg",
     favicon: "/images/favicon.ico",
     agenda: [
       {
         title: "Akad Nikah",
-        date: "2024-12-24",
+        date: "2025-06-06",
         startTime: "16:16",
         endTime: "17:30",
-        location: "Grand Ballroom, Hotel Majesty",
-        address: "Jl. Jend. Sudirman No.1, Jakarta",
+        location: "Alfamart Karangreja",
+        address: "Tobong, Kec. Cimanggu Kabupaten Cilacap",
       },
       {
         title: "Resepsi Nikah",
-        date: "2024-12-24",
+        date: "2025-06-06",
         startTime: "16:16",
         endTime: "17:30",
-        location: "Grand Ballroom, Hotel Majesty",
-        address: "Jl. Jend. Sudirman No.1, Jakarta",
+        location: "Alfmart Karangreja",
+        address: "Tobong, Kec. Cimanggu Kabupaten Cilacap",
       }
     ],
     audio: {
@@ -45,12 +45,12 @@ const config = {
       {
         bank: "Bank Central Asia",
         accountNumber: "1234567890",
-        accountName: "FULAN",
+        accountName: "Henky",
       },
       {
-        bank: "Bank Mandiri",
+        bank: "Bank Crypto",
         accountNumber: "0987654321",
-        accountName: "FULANA",
+        accountName: "Fadhli",
       }
     ]
   }
